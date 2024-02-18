d6d748f69ffea6afb63957f4d1f9ddaee05aa44d
kosch104 <konstantin-schwarze@outlook.de>
2024-02-18 22:26:37 +0100

    Update patcher system

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import shutil
 
 # Assets.HIGHWAY_NAME:0$Amity Highway&
 # $ is \r, Carriage Return, ASCII Dec 13, 0x0D = &
-def patch_file(section, filename):
+def patch_section(section, filename):
     # Section: Assets.ALLEY_NAME, Assets.STREET_NAME, Assets.HIGHWAY_NAME, Assets.BRIDGE_NAME, Assets.DAM_NAME
     # Find all entries of the section
 
@@ -29,9 +29,9 @@ def patch_file(section, filename):
         while chr(update_content[index]) in valid_chars:
             print("Found valid char: " + chr(update_content[index]) + " at index: " + str(index))
             index += 1
-        #ch = chr(update_content[index])
+        # ch = chr(update_content[index])
         invalid_chars_dollar.append(update_content[index])
-        start_index = index+1 # first index of actual name
+        start_index = index + 1  # first index of actual name
         end_index = start_index
         while chr(update_content[end_index]) in valid_chars:
             end_index += 1
@@ -84,13 +84,30 @@ def update_dir():
 """
 
 
-if __name__ == '__main__':
-    # update_dir()
-    # update_locale("en-US.loc")
-    shutil.copy("original/en-US.loc", "updated/en-US.loc")
-    patch_file("Assets.STREET_NAME", "updated/en-US.loc")
-    patch_file("Assets.HIGHWAY_NAME", "updated/en-US.loc")
-    patch_file("Assets.ALLEY_NAME", "updated/en-US.loc")
-    patch_file("Assets.BRIDGE_NAME", "updated/en-US.loc")
-    patch_file("Assets.DAM_NAME", "updated/en-US.loc")
+def patch_all_sections(locale):
+    shutil.copy("original/" + locale + ".loc", "updated/" + locale + ".loc")
+    patch_section("Assets.STREET_NAME", "updated/" + locale + ".loc")
+    patch_section("Assets.HIGHWAY_NAME", "updated/" + locale + ".loc")
+    patch_section("Assets.ALLEY_NAME", "updated/" + locale + ".loc")
+    patch_section("Assets.BRIDGE_NAME", "updated/" + locale + ".loc")
+    patch_section("Assets.DAM_NAME", "updated/" + locale + ".loc")
 
+
+def patch_all_locales():
+    patch_all_sections("en-US")
+    patch_all_sections("de-DE")
+    patch_all_sections("es-ES")
+    patch_all_sections("fr-FR")
+    patch_all_sections("it-IT")
+    patch_all_sections("ja-JP")
+    patch_all_sections("ko-KR")
+    patch_all_sections("pl-PL")
+    patch_all_sections("pt-BR")
+    patch_all_sections("ru-RU")
+    patch_all_sections("zh-HANS")
+    patch_all_sections("zh-HANT")
+
+
+if __name__ == '__main__':
+    patch_all_locales()
+    print("OK")
