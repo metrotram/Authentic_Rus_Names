a9cd0561be581f32ce76f668ebf51007b874caaf
metrotram <100780431+metrotram@users.noreply.github.com>
2024-03-07 19:01:22 +0300

    Add files via upload

---
+++ AuthenticRusNames/Patches/HighwayNameRemoverPatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Colossal.Localization;
+using Colossal.IO.AssetDatabase;
+
+namespace AuthenticRusNames.Patches
+{
+    [HarmonyPatch(typeof(LocalizationManager), "AddLocale", typeof(LocaleAsset))]
+    internal class LocalizationManager_AddLocale
+    {
+        static void Prefix(LocaleAsset asset)
+        {
+            Localization.AddCustomLocal(asset);
+        }
+    }
+}
---
+++ AuthenticRusNames/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using System.Linq;
+using BepInEx.Logging;
+
+#if BEPINEX_V6
+    using BepInEx.Unity.Mono;
+#endif
+
+namespace AuthenticRusNames
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        private void Awake()
+        {
+            Logger = base.Logger;
+
+            Logger.LogInfo("[AuthenticRusNames]: Loading Harmony patches.");
+
+            var harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MyPluginInfo.PLUGIN_GUID + "_Cities2Harmony");
+            var patchedMethods = harmony.GetPatchedMethods().ToArray();
+
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} made patches! Patched methods: " + patchedMethods.Length);
+            foreach (var patchedMethod in patchedMethods)
+            {
+                Logger.LogInfo($"[AuthenticRusNames]: Patched method: {patchedMethod.Module.Name}:{patchedMethod.Name}");
+            }
+        }
+    }
+}
